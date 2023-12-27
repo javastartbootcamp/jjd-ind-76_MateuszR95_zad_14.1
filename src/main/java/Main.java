8faa2fe92741b67faa33382dcdae81031175c650
Mateusz Rojek <mateuszr2047@gmail.com>
2023-12-27 13:09:09 +0100

    commit1

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,8 +1,17 @@
+import java.io.FileNotFoundException;
+import java.util.Map;
+import java.util.Set;
+
 public class Main {
-
-
     public static void main(String[] args) {
 
+        String fileName = "liczby.txt";
+        try {
+            Map<Integer, Integer> numbersMap = NumbersFileReader.fillMapFromFile(fileName);
+            NumbersFileReader.printResult(numbersMap);
+        } catch (FileNotFoundException e) {
+            System.out.println("Nie znaleziono pliku do odczytu");
+        }
     }
 
 }
